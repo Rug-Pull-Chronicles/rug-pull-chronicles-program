903a9438b1a0f4466674d3d9fc94e4de35bce62c
Mattia <mp.web3@gmail.com>
2025-04-25 17:15:31 +0200

    update to test metaplex-core-example first

--- programs/rug-pull-chronicles-program/src/lib.rs
+++ programs/rug-pull-chronicles-program/src/lib.rs
@@ -1,21 +1,45 @@
-pub mod constants;
-pub mod error;
-pub mod instructions;
-pub mod state;
+// pub mod constants;
+// pub mod error;
+// pub mod instructions;
+// pub mod state;
+
+// use anchor_lang::prelude::*;
+
+// pub use constants::*;
+// pub use instructions::*;
+// pub use state::*;
+
+// declare_id!("AisPtd8Pkci6V6x38MeZHBd5i7riJU8jCuHKtLLThCNM");
+
+// #[program]
+// pub mod rug_pull_chronicles_program {
+//     use super::*;
+
+//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+//         initialize::handler(ctx)
+//     }
+// }
 
 use anchor_lang::prelude::*;
 
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
+declare_id!("9Tc3PoSyNr8JJ2Q47AyovJiihSwXQM2eKTAUf4szziMu");
 
-declare_id!("AisPtd8Pkci6V6x38MeZHBd5i7riJU8jCuHKtLLThCNM");
+mod instructions;
+use instructions::*;
 
 #[program]
-pub mod rug_pull_chronicles_program {
+pub mod metaplex_core_example {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn mint_asset(ctx: Context<MintAsset>) -> Result<()> {
+        ctx.accounts.mint_core_asset()
     }
 }
+
+#[derive(Accounts)]
+pub struct Initialize {}
---
+++ tests/metaplex-core-example.ts
@@ -0,0 +1,95 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { MetaplexCoreExample } from "../target/types/rug_pull_chronicles_program";
+
+import wallet from "../Turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { createPluginV2, createV1, fetchAssetV1, mplCore, pluginAuthority, MPL_CORE_PROGRAM_ID, createCollection } from "@metaplex-foundation/mpl-core";
+import { base58, createSignerFromKeypair, generateSigner, signerIdentity, sol } from "@metaplex-foundation/umi";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
+import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
+
+const umi = createUmi("http://127.0.0.1:8899").use(mplCore());
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+umi.use(signerIdentity(signer));
+
+const collectionAsset = generateSigner(umi);
+const asset = generateSigner(umi);
+
+describe("metaplex-core-example", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.MetaplexCoreExample as Program<MetaplexCoreExample>;
+
+  it("Request Airdrop", async () => {
+    let airdrop1 = await umi.rpc.airdrop(keypair.publicKey, sol(10));
+    console.log(airdrop1);
+  });
+
+  it("Create a Collection Asset", async() => {
+    const result = await createCollection(umi, {
+      collection: collectionAsset,
+      name: 'My Collection',
+      uri: 'https://example.com/my-collection.json',
+    }).sendAndConfirm(umi);
+
+    console.log("\nCollection Asset minted. Transaction signature: ", base58.deserialize(result.signature)[0])
+  });
+
+  it("Create an Asset", async () => {
+    const result = await createV1(umi, {
+      asset: asset,
+      name: 'My Nft',
+      uri: 'https://example.com/my-nft',
+      collection: collectionAsset.publicKey,
+      // MPL Core program doesn't allow you to specify both a collection and an updateAuthority at the same time
+      // updateAuthority: keypair.publicKey,
+      plugins: [
+        {
+          plugin: createPluginV2({
+            type: 'Attributes',
+            attributeList: [{ key: 'key', value: 'value' }],
+          }),
+          authority: pluginAuthority('UpdateAuthority'),
+        },
+      ],
+    }).sendAndConfirm(umi);
+
+    console.log("\nAsset minted. Transaction signature: ", base58.deserialize(result.signature)[0])
+  });
+
+  it("Fetch an Asset", async () => {
+    const fetchedAsset = await fetchAssetV1(umi, asset.publicKey)
+
+    console.log("\nAsset fetched:\n", fetchedAsset)
+  });
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("\nYour transaction signature", tx);
+  });
+
+  it("Mint Core Asset", async () => {
+    const asset = Keypair.generate();
+
+    console.log("\nAsset address: ", asset.publicKey.toBase58());
+
+    const tx = await program.methods.mintAsset()
+      .accountsPartial({
+        user: provider.publicKey,
+        mint: asset.publicKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        mplCoreProgram: MPL_CORE_PROGRAM_ID,
+      })
+      .signers([asset])
+      .rpc();
+
+    console.log("\nYour transaction signature", tx);
+  });
+});
